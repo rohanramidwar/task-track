f681d30ed00f6b079936382cd37199b143c7f3e8
RohanRamidwar <rohanramidwar71@gmail.com>
2023-07-26 23:54:07 +0530

    added remove task functionality

--- scripts/todo.js
+++ scripts/todo.js
@@ -3,29 +3,57 @@
 const todoListContainer = document.getElementById("todoListContainer")
 const iTask = document.getElementById("iTask")
 
+let todoContainer
+let taskComplete
+let task
+let rmTask
+
 function addTask()
 {
+    if(iTask.value === "")
+    {
+        alert("Add a task first!!")
+    }
+    else
+    {
 
-    let todoContainer = document.createElement("li")
-    todoContainer.classList.add("todoContainer")
-    todoListContainer.appendChild(todoContainer)
-
-    let taskComplete = document.createElement("span")
-    taskComplete.classList.add("material-symbols-outlined")
-    taskComplete.innerHTML = "circle"
-    todoContainer.appendChild(taskComplete)
-
-    let task = document.createElement("span")
-    task.classList.add("task")
-    task.innerHTML = iTask.value
-    todoContainer.appendChild(task)
-
-    let rmTask = document.createElement("span")
-    rmTask.classList.add("material-symbols-outlined")
-    rmTask.classList.add("rm-task")
-    rmTask.innerHTML = "close"
-    todoContainer.appendChild(rmTask)
-
+        todoContainer = document.createElement("li")
+        todoContainer.classList.add("todoContainer")
+        todoListContainer.appendChild(todoContainer)
+    
+        taskComplete = document.createElement("span")
+        taskComplete.classList.add("material-symbols-outlined")
+        taskComplete.innerHTML = "circle"
+        todoContainer.appendChild(taskComplete)
+    
+        task = document.createElement("span")
+        task.classList.add("task")
+        task.innerHTML = iTask.value
+        todoContainer.appendChild(task)
+    
+        rmTask = document.createElement("span")
+        rmTask.classList.add("material-symbols-outlined")
+        rmTask.classList.add("rm-task")
+        rmTask.setAttribute("onclick", "deleteTask()")
+        rmTask.innerHTML = "close"
+        todoContainer.appendChild(rmTask)
+    
+    }
     iTask.value = ""
 }
+
+todoListContainer.addEventListener("click", function(e)
+{
+    if(e.target.classList.contains('rm-task'))
+    {
+        e.target.parentElement.remove()
+    }
+})
+
+
+
+
+
+
+
 
