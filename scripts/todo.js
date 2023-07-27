9a58ff8c925627694c290bfed1b58de2fe5d1d5d
RohanRamidwar <rohanramidwar71@gmail.com>
2023-07-27 16:37:34 +0530

    1

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,12 +1,6 @@
-// alert("Linked todo.js")
 
 const todoListContainer = document.getElementById("todoListContainer")
 const iTask = document.getElementById("iTask")
-
-let todoContainer
-let taskComplete
-let task
-let rmTask
 
 function addTask()
 {
@@ -16,44 +10,47 @@ function addTask()
     }
     else
     {
-
-        todoContainer = document.createElement("li")
+        // add task 
+        let todoContainer = document.createElement("li")
         todoContainer.classList.add("todoContainer")
         todoListContainer.appendChild(todoContainer)
-    
-        taskComplete = document.createElement("span")
+         
+        let taskComplete = document.createElement("span")
         taskComplete.classList.add("material-symbols-outlined")
         taskComplete.innerHTML = "circle"
         todoContainer.appendChild(taskComplete)
     
-        task = document.createElement("span")
+        let task = document.createElement("span")
         task.classList.add("task")
         task.innerHTML = iTask.value
         todoContainer.appendChild(task)
     
-        rmTask = document.createElement("span")
+        let rmTask = document.createElement("span")
         rmTask.classList.add("material-symbols-outlined")
         rmTask.classList.add("rm-task")
-        rmTask.setAttribute("onclick", "deleteTask()")
         rmTask.innerHTML = "close"
-        todoContainer.appendChild(rmTask)
-    
+        todoContainer.appendChild(rmTask) 
     }
     iTask.value = ""
 }
 
+function addBtnActive()
+{
+    document.querySelector(".add-btn").style.color = "#3459c1"
+    document.querySelector(".add-btn").style.border = "1px solid #78BAFD"
+}
+
+
 todoListContainer.addEventListener("click", function(e)
 {
-    if(e.target.classList.contains('rm-task'))
+    // delete task 
+    if(e.target.classList.contains("rm-task"))
     {
         e.target.parentElement.remove()
     }
+    // task complete 
+    else if(e.target.classList.contains("task"))
+    {
+        e.target.classList.toggle("line-through")
+    }
 })
-
-
-
-
-
-
-
-
